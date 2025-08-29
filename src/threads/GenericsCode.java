ca1cd3064f9bd598a58bd9d1a240fb65adcb197b
luffy-5482 <tayyabsirkazi@gmail.com>
2025-08-29 09:37:51 +0530

    end of core java

---
+++ src/threads/GenericsCode.java
@@ -0,0 +1,21 @@
+package threads;
+
+import java.util.ArrayList;
+
+import has_a_relationship.new1;
+
+public class GenericsCode {
+	public static void main(String[] args) {
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		ArrayList al1 = new ArrayList();
+		al.add(100);
+		al.add("Sachin");
+		al.add(100);
+		al.add('s');
+		al.add(100);
+		al.add(100.768f);
+		al.add(3456.789023456);
+		al.add(t);
+		System.out.println(al);
+	}
+}
